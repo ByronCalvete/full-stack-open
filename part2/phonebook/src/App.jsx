802c170feb046e8418fb42ccb430bae628ae597b
Byron Calvete <calvete94@gmail.com>
2023-03-28 11:54:18 -0300

    add: exercise 2.8: phonebook, step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,39 +1,71 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }])
   const [newPerson, setNewPerson] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const newPersonToAdd = {
-      name: newPerson.trim()
+      name: newPerson.trim(),
+      number: newNumber
     }
 
     const findPersonInContacts = persons.some(person => person.name === newPersonToAdd.name)
     if (findPersonInContacts) {
       alert(`${newPersonToAdd.name} is already added to phonebook`)
       setNewPerson('')
+      setNewNumber('')
       return
     }
 
     setPersons([...persons, newPersonToAdd])
     setNewPerson('')
+    setNewNumber('')
   }
 
-  const handleChange = (e) => {
+  const handleChangeName = (e) => {
     setNewPerson(e.target.value)
+  }
+
+  const handleChangeNumber = (e) => {
+    setNewNumber(e.target.value)
   }
 
   return (
     <>
       <h1>Phonebook</h1>
       <form onSubmit={handleSubmit}>
-        <input value={newPerson} onChange={handleChange}/>
+        <table>
+          <tbody>
+            <tr>
+              <td>name:</td>
+              <td>
+                <input
+                  value={newPerson}
+                  onChange={handleChangeName}
+                  placeholder='Byron Calvete'
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>number:</td>
+              <td>
+                <input
+                  type='number'
+                  value={newNumber}
+                  onChange={handleChangeNumber}
+                  placeholder='1234567890'
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
         <button type='submit'>add</button>
       </form>
       <h2>Numbers</h2>
-      {persons.map((phoneNumber, i) => <p key={i}>{phoneNumber.name}</p>)}
+      {persons.map((person) => <p key={person.number}>{person.name} {person.number.slice(0,3)}-{person.number.slice(4)}</p>)}
     </>
   )
 }
