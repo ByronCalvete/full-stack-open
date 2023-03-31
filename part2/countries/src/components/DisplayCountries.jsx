ce2ce5016518708bc37ac55128aacd5aae7b0ea7
Byron Calvete <calvete94@gmail.com>
2023-03-31 12:29:27 -0300

    add: exercise 2.19: countries, step2

--- part2/countries/src/components/DisplayCountries.jsx
+++ part2/countries/src/components/DisplayCountries.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react'
 import CountryDetail from "./CountryDetail"
 
 const DisplayCountries = ({ countries }) => {
+  const [showCountry, setShowCountry] = useState(null)
+
+  const handleClick = (country) => {
+    setShowCountry(country)
+  }
+
   return (
     <>
       {
@@ -8,9 +15,12 @@ const DisplayCountries = ({ countries }) => {
         ? 'Too many matches, specify another filter'
         : countries.length > 1
           ? (countries.map(country => (
-             <p key={country.name.common}>{country.name.common}</p>
-            )))
+             <p key={country.name.common}>{country.name.common} <button onClick={() => handleClick(country)}>show</button></p>
+          )))
           : <CountryDetail country={countries[0]}/>
+      }
+      {
+        showCountry && <CountryDetail country={showCountry} />
       }
     </>
   )
