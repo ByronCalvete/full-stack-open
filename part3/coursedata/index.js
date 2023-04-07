a035468e28129d7ff11256c41bd7c74f50581a96
Byron Calvete <calvete94@gmail.com>
2023-04-07 07:19:19 -0300

    add: coursedata express use

--- part3/coursedata/index.js
+++ part3/coursedata/index.js
@@ -1,28 +1,79 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 let notes = [
   {
     id: 1,
-    content: 'HTML is easy',
+    content: "HTML is easy",
     important: true
   },
   {
     id: 2,
-    content: 'Browser can execute only JavaScript',
+    content: "Browser can execute only JavaScript",
     important: false
   },
   {
     id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
+    contene: "GET and POST are the most important methods of HTTP protocol",
     important: true
   }
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+app.get('/', (request, response) => {
+  response.send('<h1>Hi Byron J Calvete</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if(note) {
+    response.json(note)
+  } else {
+    response.statusMessage = `There are no resources corresponding to the note with id number ${id}`
+    response.status(404).end()
+  }
+})
+
+const generatedId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(note => note.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if(!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    id: generatedId(),
+    content: body.content,
+    important: body.important || false
+  }
+
+  notes = [...notes, note]
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Serve running on port ${PORT}`)
+})
