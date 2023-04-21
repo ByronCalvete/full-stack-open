ce7054cccc13ac4b4854bbae0d3e6728bc6aa6ee
Byron Calvete <calvete94@gmail.com>
2023-04-21 17:35:29 -0300

    fix: url to my own backend in local

--- part2/coursedata/src/services/notes.js
+++ part2/coursedata/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
 // Async await
 export const getAll = async () => {
   const request =  axios.get(baseUrl)
