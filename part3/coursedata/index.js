e63519154494bbee4c5b582c8e0920c24480a285
Byron Calvete <calvete94@gmail.com>
2023-04-21 17:52:14 -0300

    add: PORT to env variable for deploy

--- part3/coursedata/index.js
+++ part3/coursedata/index.js
@@ -102,7 +102,7 @@ app.put('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Serve running on port ${PORT}`)
 })
