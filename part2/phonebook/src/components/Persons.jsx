bab8baf6f9876662d76aaa0f508baea9f958ba1f
Byron Calvete <calvete94@gmail.com>
2023-04-24 12:40:59 -0300

    fix: persons number format

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -6,7 +6,7 @@ const Persons = ({ persons, filtered, handleClickDelete }) => {
           .filter(person => person.name.toLowerCase().includes(filtered.toLowerCase()))
           .map((person) => (
             <p key={person.id}>
-              {person.name} {person.number.slice(0,3)}-{person.number.slice(3)} <button onClick={() => handleClickDelete(person.id)}>delete</button>
+              {person.name} {person.number} <button onClick={() => handleClickDelete(person.id)}>delete</button>
             </p>
           ))
       }
