7060a652b92e2c6c576d9f3bd7ba824fa863b3c8
Byron Calvete <calvete94@gmail.com>
2023-04-24 12:46:29 -0300

    add: exercise 3.13: Phonebook database, step1

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
+
+const app = express()
 
 app.use(express.json())
 app.use(cors())
@@ -13,35 +15,14 @@ morgan.token('data', (request) => {
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :data'))
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122'
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>This is a phonebook application backend</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+mongoose.set('strictQuery', false)
+
+mongoose.connect(process.env.MONGODB_CONNECTION)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
