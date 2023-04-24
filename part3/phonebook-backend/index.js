e0fd4ce9ba556ad00edc6cb2a9714a713988ac9a
Byron Calvete <calvete94@gmail.com>
2023-04-24 19:58:48 -0300

    add: exercise 3.18: Phonebook database, step6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -36,16 +36,16 @@ app.get('/info', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if(person) {
-    response.json(person)
-  } else {
-    response.statusMessage = `There are no sources corresponding to the person with the id number ${id}` 
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
