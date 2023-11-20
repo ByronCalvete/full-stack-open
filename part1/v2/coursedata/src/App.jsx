d00d0a15409fb50c63b28e400dd41f6fd146ac52
Byron J Calvete <calvete94@gmail.com>
2023-11-20 14:02:03 +0100

    add: coursedata - JavaScript & Component state, event handlers

--- part1/v2/coursedata/src/App.jsx
+++ part1/v2/coursedata/src/App.jsx
@@ -1,9 +1,35 @@
+import { useState } from 'react'
+
+import Display from './components/Display'
+import Button from './components/Button'
+
+const log = (text, variable) => console.log(text, variable)
+
 const App = () => {
-  const friends = [ 'Peter', 'Maya']
+  const [counter, setCounter] = useState(0)
+  log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    log('decreasing, value before', counter)
+    counter > 0 && setCounter(counter - 1)
+  }
+
+  const handleReset = () => {
+    log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <>
-      <p>{friends}</p>
+      <Display counter={counter}/>
+      <Button label='plus' handleClick={increaseByOne}/>
+      <Button label='minus' handleClick={decreaseByOne}/>
+      <Button label='reset' handleClick={handleReset}/>
     </>
   )
 }
