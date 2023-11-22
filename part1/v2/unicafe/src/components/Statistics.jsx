492ed8d2b9b8096abc93b665ef17a3e15dd90944
Byron J Calvete <calvete94@gmail.com>
2023-11-22 12:20:24 +0100

    add exercise 1.7 - unicafe, step2

--- part1/v2/unicafe/src/components/Statistics.jsx
+++ part1/v2/unicafe/src/components/Statistics.jsx
@@ -1,9 +1,16 @@
 const Statistics = ({ good, neutral, bad }) => {
+  const totalFeedback = good + neutral + bad
+  const average = (good - bad)/totalFeedback
+  const positive = (good / totalFeedback)*100
+
   return(
     <>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {totalFeedback}</p>
+      <p>Average {average ? average : 0}</p>
+      <p>Positive {positive ? positive : 0}%</p>
     </>
   )
 }
