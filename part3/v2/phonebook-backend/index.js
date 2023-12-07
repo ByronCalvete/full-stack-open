b35754570de55eecee160fe80296f76c165436ce
Byron J Calvete <calvete94@gmail.com>
2023-12-08 00:38:27 +0100

    add: exercise 3.5 - phonebook backend, step5

--- part3/v2/phonebook-backend/index.js
+++ part3/v2/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express') 
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -50,6 +52,38 @@ app.get('/api/persons/:id', (request, response) => {
     response.statusMessage = `Not person with id ${id}`
     response.status(404).end()
   }
+})
+
+const generateId = () => {
+  const maxId  = persons.length > 0
+    ? Math.max(...persons.map(person => person.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'Name missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'Number missing'
+    })
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = [ ...persons, newPerson ]
+  response.status(201).json(newPerson)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
