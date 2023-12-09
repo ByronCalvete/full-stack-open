b29463839c88f96be5dcbea618b8dfdd4593b80f
Byron J Calvete <calvete94@gmail.com>
2023-12-09 23:31:22 +0100

    add: exercise 3.9 - phonebook backend, step9

--- part3/v2/phonebook-backend/index.js
+++ part3/v2/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express') 
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
@@ -9,6 +10,7 @@ morgan.token('data', (request) => {
 })
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :data'))
+app.use(cors())
 
 let persons = [
   {
