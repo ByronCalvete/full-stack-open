f3ee989c2c4ca9189b640e17711b09a605f27fb4
Byron J Calvete <calvete94@gmail.com>
2023-12-14 00:18:45 +0100

    add: Validation and ESLint, part1

--- part3/v2/coursedata/index.js
+++ part3/v2/coursedata/index.js
@@ -43,7 +43,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     })
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -57,9 +57,13 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false
   })
 
-  newNote.save().then(savedNote => {
-    response.status(201).json(savedNote)
-  })
+  newNote.save()
+    .then(savedNote => {
+      response.status(201).json(savedNote)
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.put('/api/notes/:id', (request, response) => {
@@ -71,7 +75,7 @@ app.put('/api/notes/:id', (request, response) => {
     important: body.important
   }
 
-  Note.findByIdAndUpdate(id, note, { new: true })
+  Note.findByIdAndUpdate(id, note, { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -99,6 +103,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
