171dac68697de3e35554548058341d3201919f47
Byron J Calvete <calvete94@gmail.com>
2023-12-21 23:45:29 +0100

    add: exercise 4.4 - helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dumy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs = []) => {
+  const likes = blogs
+    .map(blog => blog.likes)
+    .reduce((sum, item) => sum + item, 0)
+
+  return likes
+}
+
 module.exports = {
-  dumy
+  dumy,
+  totalLikes
 }
