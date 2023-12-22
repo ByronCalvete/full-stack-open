4e61a7e7f892ab1126c9e51891da8d95e8d809d3
Byron J Calvete <calvete94@gmail.com>
2023-12-22 19:20:47 +0100

    update: favoriteBlog test

--- part4/bloglist/tests/favoriteBlog.test.js
+++ part4/bloglist/tests/favoriteBlog.test.js
@@ -7,6 +7,15 @@ describe('favorite blog', () => {
     expect(result).toBe('No blogs yet')
   })
 
+  test('when only has one blog', () => {
+    const result = listHelper.favoriteBlog([blogs[0]])
+    expect(result).toEqual({
+      title: 'React patterns',
+      author: 'Michael Chan',
+      likes: 7
+    })
+  })
+
   test('when find the favorite blog', () => {
     const result = listHelper.favoriteBlog(blogs)
     expect(result).toEqual({
