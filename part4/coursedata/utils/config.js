01473bd2fe85557e43e77aa8b6b763a0c8692c41
Byron J Calvete <calvete94@gmail.com>
2023-12-25 18:14:10 +0100

    fix: add dotenv library instead the new native config.env in node v20

--- part4/coursedata/utils/config.js
+++ part4/coursedata/utils/config.js
@@ -1,3 +1,7 @@
+// We use this method with dependency because jest not support the new native 'config.env' implementation yet.
+const dotenv = require('dotenv')
+dotenv.config()
+
 const PORT = process.env.PORT
 
 const MONGODB_URI = process.env.NODE_ENV === 'test'
