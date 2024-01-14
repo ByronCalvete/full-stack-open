4aad1249cf3935cdc30903178479ed45d7bc21e3
Byron J Calvete <calvete94@gmail.com>
2024-01-14 21:11:25 +0100

    add: User administration - part2

--- part4/coursedata/models/user.js
+++ part4/coursedata/models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
   name: String,
   passwordHash: String,
   notes: [
@@ -12,11 +17,13 @@ const userSchema = new mongoose.Schema({
   ]
 })
 
+userSchema.plugin(uniqueValidator)
+
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
-    delete returnedObject.__V
+    delete returnedObject.__v
     // the passwordHash should not be revelated
     delete returnedObject.passwordHash
   }
