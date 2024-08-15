7c92a0aeeb10d39e70a3e3671a7fcbfe7abfdc10
Byron J Calvete <calvete94@gmail.com>
2024-08-15 23:44:16 +0200

    add: Component state, event handlers

--- part1/v3/coursedata/src/App.jsx
+++ part1/v3/coursedata/src/App.jsx
@@ -1,23 +1,39 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </>
-  )
-}
+import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+
+  console.log(`rendering with counter value ${counter}`)
+
+  const increaseByOne = () => {
+    console.log(`Increasing, value before ${counter}`)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log(`Decreasing, value before ${counter}`)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log(`resetting to zero, value before ${counter}`)
+    setCounter(0)
+  }
 
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26+10}/>
-      <Hello name={name} age={age}/>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='plus' />
+      <Button onClick={setToZero} text='reset' />
+      <Button onClick={decreaseByOne} text='minus' />
     </>
   )
 }
