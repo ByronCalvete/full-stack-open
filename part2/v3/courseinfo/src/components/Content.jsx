98d4eb764681a0aa81763b24d14b333be67f5c02
Byron J Calvete <calvete94@gmail.com>
2024-08-19 23:36:31 +0200

    exercise: 2.2 & 2.3 Course Information, step7 & step8

--- part2/v3/courseinfo/src/components/Content.jsx
+++ part2/v3/courseinfo/src/components/Content.jsx
@@ -1,9 +1,14 @@
 import Part from './Part'
 
 const Content = ({ parts }) => {
+  const total = parts.reduce((sum, part) => {
+    return sum + part.exercises
+  }, 0)
+
   return (
     <>
       {parts.map(part => <Part key={part.id} part={part}/>)}
+      <p><strong>total of {total} exercises</strong></p>
     </>
   )
 }
