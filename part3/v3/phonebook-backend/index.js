2283e2684a724fffc691f7778798772c9dbbf70c
Byron J Calvete <calvete94@gmail.com>
2024-08-24 15:11:10 +0200

    exercise: 3.8 Phonebook backend, step8

--- part3/v3/phonebook-backend/index.js
+++ part3/v3/phonebook-backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const app = express()
-
 const morgan = require('morgan')
 
+const app = express()
+
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (request, response) => {
+  return request.method === 'POST' && JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
 let persons = [
   {
@@ -67,7 +71,6 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  console.log(body)
 
   if (!body.name || !body.number) {
     return response.status(400).json({
