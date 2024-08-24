6944a582b65113cab44e151a4df4fb047693d3f7
Byron J Calvete <calvete94@gmail.com>
2024-08-24 13:58:49 +0200

    exercise: 3.5 Phonebook backend, step5

--- part3/v3/phonebook-backend/index.js
+++ part3/v3/phonebook-backend/index.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456"
   },
   {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
   },
   {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345"
   },
   {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
   }
 ]
 
@@ -50,6 +52,28 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(person => Number(person.id)))
+    : 0
+  
+  return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = [ ...persons, person ]
+  response.status(201).json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
