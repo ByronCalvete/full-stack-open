f9b2d2751d0495caa8b4a52baa34d71638d0db43
Byron J Calvete <calvete94@gmail.com>
2024-08-26 18:53:20 +0200

    exercise: 3.9 Phonebook backend, step9

--- part3/v3/phonebook-backend/index.js
+++ part3/v3/phonebook-backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (request, response) => {
   return request.method === 'POST' && JSON.stringify(request.body)
