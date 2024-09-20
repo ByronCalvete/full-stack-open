8bff9d5ea5efe90e3e8c7d996f490fb5260fa78d
Byron J Calvete <calvete94@gmail.com>
2024-09-20 23:59:26 +0200

    exercise: 5.7 Blog List Frontend, step7

--- part5/v3/bloglist-frontend/src/components/Blog.jsx
+++ part5/v3/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [ showDetails, setShowDetails ] = useState(true)
+
+  const handleClick = () => setShowDetails(!showDetails)
+
+  return (
+    <div className='blog'>
+      {
+        showDetails
+          ? (<>
+              <span>{blog.title}</span>
+              <button onClick={handleClick}>view</button>
+            </>)
+          : (<>
+              <span>{blog.title} {blog.author}</span>
+              <button onClick={handleClick}>hide</button>
+              <br/>
+              <span>{blog.url}</span>
+              <br/>
+              <span>likes {blog.likes} <button>like</button></span>
+              <br/>
+              <span>{blog.author}</span>
+            </>)
+      }
+    </div>
+  ) 
+}
 
 export default Blog
