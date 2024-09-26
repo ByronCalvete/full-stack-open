01c086dda3a6e1063e610b4d40523b4b03d3e444
Byron J Calvete <calvete94@gmail.com>
2024-09-26 21:33:27 +0200

    exercise: 5.14 Blog List Tests, step2

--- part5/v3/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/v3/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('blog render title and author by default', () => {
@@ -6,7 +7,12 @@ test('blog render title and author by default', () => {
     title: 'I am a title',
     author: 'I am an author',
     url: 'I am an url',
-    likes: 2
+    likes: 2,
+    user: {
+      username: 'username',
+      name: 'name',
+      id: '2263jhgfjk234jhg'
+    }
   }
 
   render(<Blog blog={blog} />)
@@ -14,8 +20,41 @@ test('blog render title and author by default', () => {
   const titleAuthor = screen.getByText(`${blog.title} ${blog.author}`)
   expect(titleAuthor).toBeDefined()
 
-  const url = screen.queryByText(blog.url)
-  const likes = screen.queryByText(blog.likes)
+  const url = screen.queryByText(`${blog.url}`)
+  const likes = screen.queryByText(`${blog.likes}`)
   expect(url).toBeNull()
   expect(likes).toBeNull()
+})
+
+test('check url and likes when click the view button', async () => {
+  const blog = {
+    title: 'I am a title',
+    author: 'I am an author',
+    url: 'I am an url',
+    likes: 2,
+    user: {
+      username: 'username',
+      name: 'name',
+      id: '2263jhgfjk234jhg'
+    }
+  }
+  const user = {
+    name: 'The name',
+    username: 'The username',
+    toke: 'thetoken'
+  }
+
+  const { container } = render(<Blog blog={blog} user={user} />)
+
+  const userClick = userEvent.setup()
+  const button = screen.getByText('view')
+  await userClick.click(button)
+
+  const titleAuthor = container.querySelector('.title-author')
+  const url = container.querySelector('.url')
+  const likes = container.querySelector('.likes')
+
+  expect(titleAuthor).toHaveTextContent(`${blog.title} ${blog.author}`)
+  expect(url).toHaveTextContent(`${blog.url}`)
+  expect(likes).toHaveTextContent(`likes ${blog.likes}`)
 })
