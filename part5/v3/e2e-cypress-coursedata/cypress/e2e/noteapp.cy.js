dee67cd4df999650562aedcebf828f6922d3fd0d
Byron J Calvete <calvete94@gmail.com>
2024-10-08 23:58:40 +0200

    add: End to End testing: Cypress, part3

--- part5/v3/e2e-cypress-coursedata/cypress/e2e/noteapp.cy.js
+++ part5/v3/e2e-cypress-coursedata/cypress/e2e/noteapp.cy.js
@@ -1,5 +1,12 @@
 describe('Note App', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Root',
+      username: 'root',
+      password: 'sekret'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -8,12 +15,60 @@ describe('Note App', function() {
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2024')
   })
 
-  it('login form can be opened', function() {
+  it('user can log in', function() {
     cy.contains('log in').click()
     cy.get('#username').type('root')
     cy.get('#password').type('sekret')
     cy.get('#login-button').click()
 
     cy.contains('Root logged-in')
+  })
+
+  it('login fails with wrong password', function() {
+    cy.contains('log in').click()
+    cy.get('#username').type('root')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+      .should('contain', 'Wrong Credentials')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+
+    cy.get('html').should('not.contain', 'Root logged-in')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('root')
+      cy.get('#password').type('sekret')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+
+      cy.contains('a note created by cypress')
+    })
+
+    describe('and a note exits', function() {
+      beforeEach(function() {
+        cy.contains('new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+
+      it('it can be made not important', function() {
+        cy.contains('another note cypress')
+          .contains('make not important')
+          .click()
+        
+        cy.contains('another note cypress')
+          .contains('make important')
+      })
+    })
   })
 })
