b388b6d1bbdf7f605c78c1b8a6454b0ae1399080
Byron J Calvete <calvete94@gmail.com>
2024-11-08 10:24:49 +0100

    exercise: 6.2 Unicafe Revisited, step2

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,6 +1,32 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createStore } from 'redux'
+import counterReducer from './reducers/reducer'
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button>
+        <button onClick={() => store.dispatch({ type: 'OK' })}>ok</button>
+        <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
+        <button onClick={() => store.dispatch({ type: 'ZERO' })}>reset stats</button>
+      </div>
+      <div>
+        {Object.entries(store.getState()).map(stat => (
+          <p key={stat[0]}>{stat[0]}: {stat[1]}</p>)
+        )}
+      </div>
+    </>
+  )
+}
+
+const root = createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
