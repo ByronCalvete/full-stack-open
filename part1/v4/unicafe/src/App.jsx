647dcf685570639c7654559259dff207e8537fa8
Byron J Calvete <calvete94@gmail.com>
2025-06-05 16:52:48 +0200

    add: exercise 1.11 unicafe, step 6

--- part1/v4/unicafe/src/App.jsx
+++ part1/v4/unicafe/src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>
 
-const StatisticLine = ({ text, value, units }) => <p>{text} {value}{units}</p>
+const StatisticLine = ({ text, value, units }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}{units}</td>
+  </tr>
+)
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
@@ -16,14 +21,16 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <>
       <h2>Statistics</h2>
-      <div>
-        <StatisticLine text='good' value={good}/>
-        <StatisticLine text='neutral' value={neutral}/>
-        <StatisticLine text='bad' value={bad}/>
-        <StatisticLine text='all' value={all} />
-        <StatisticLine text='average' value={all ? average : 0} />
-        <StatisticLine text='positive' value={all ? positive : 0} units='%'/>
-      </div>
+      <table>
+        <tbody>  
+          <StatisticLine text='good' value={good}/>
+          <StatisticLine text='neutral' value={neutral}/>
+          <StatisticLine text='bad' value={bad}/>
+          <StatisticLine text='all' value={all} />
+          <StatisticLine text='average' value={all ? average : 0} />
+          <StatisticLine text='positive' value={all ? positive : 0} units='%'/>
+        </tbody>
+      </table>
     </>
   )
 }
