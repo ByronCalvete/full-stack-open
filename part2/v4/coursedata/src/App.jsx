f6318c8302f3faae3861ada98da77b2738ac91c3
Byron J Calvete <calvete94@gmail.com>
2025-06-07 23:30:27 +0200

    add: Forms

--- part2/v4/coursedata/src/App.jsx
+++ part2/v4/coursedata/src/App.jsx
@@ -1,17 +1,55 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [ notes, setNotes ] = useState(props.notes)
+  const [ newNote, setNewNote ] = useState('')
+  const [ showAll, setShowAll ] = useState(true)
+
+  const addNote = (e) => {
+    e.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random < 0.5,
+      id: String(notes.length + 1)
+    }
+
+    setNotes([ ...notes, noteObject ])
+    setNewNote('')
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note
             key={note.id}
             note={note}
           />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleChange}
+        />
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
