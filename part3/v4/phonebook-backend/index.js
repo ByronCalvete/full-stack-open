02fc6b41e93463e6121c0f492a70f79b06ffec9a
Byron J Calvete <calvete94@gmail.com>
2025-06-13 00:36:20 +0200

    add: exercise 3.8 Phonebook backend, step 8

--- part3/v4/phonebook-backend/index.js
+++ part3/v4/phonebook-backend/index.js
@@ -27,7 +27,11 @@ let persons = [
   }
 ]
 
-app.use(morgan('tiny'))
+morgan.token('body', (request, response) => {
+  return request.method === 'POST' && JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (request, response) => {
   const content = `<div>
