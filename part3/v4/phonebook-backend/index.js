40d2107e05ca9bf1753ca888207ced533e3b9ef7
Byron J Calvete <calvete94@gmail.com>
2025-06-12 23:33:22 +0200

    add: exercise 3.5 Phonebook backend, step 5

--- part3/v4/phonebook-backend/index.js
+++ part3/v4/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: "1",
@@ -46,6 +48,27 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(person => Number(person.id)))
+    : 0
+  
+  return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = [ ...persons, newPerson ]
+  response.status(201).json(newPerson)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
