ba6dc8e69b5b322d16dec1a918de142b75ad6eae
Byron J Calvete <calvete94@gmail.com>
2025-06-19 19:36:28 +0200

    add: Validation and ESLint, part1

--- part3/v4/coursedata/index.js
+++ part3/v4/coursedata/index.js
@@ -59,7 +59,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -77,6 +77,7 @@ app.post('/api/notes', (request, response) => {
     .then(savedNote => {
       response.status(201).json(savedNote)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -121,6 +122,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
