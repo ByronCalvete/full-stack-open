218aa19106a75ac120d9d985a8e05976ff41bec7
Byron J Calvete <calvete94@gmail.com>
2025-07-03 18:05:51 +0200

    add: exercise 5.3 Blog List Frontend, step3

--- part5/v4/bloglist-frontend/src/App.jsx
+++ part5/v4/bloglist-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 
 import BlogList from './components/BlogList'
 import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
 import blogService from './services/blogs'
 
 const App = () => {
@@ -16,9 +17,27 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('blogListUserLogged')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = (userLogged) => {
     window.localStorage.setItem('blogListUserLogged', JSON.stringify(userLogged))
+    blogService.setToken(userLogged.token)
     setUser(userLogged)
+  }
+
+  const addBlog = (blogObject) => {
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        setBlogs([ ...blogs, returnedBlog ])
+      })
   }
 
   const handleLogout = () => {
@@ -34,6 +53,7 @@ const App = () => {
           ? <LoginForm logUser={handleLogin}/>
           : <div>
               <p>{user.name} logged-in <button onClick={handleLogout}>Logout</button></p>
+              <BlogForm createBlog={addBlog}/>
               <BlogList blogs={blogs} />
             </div>
       }
