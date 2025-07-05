7ae88a0aa55b41a062256b507581888fd7b117de
Byron J Calvete <calvete94@gmail.com>
2025-07-05 16:18:59 +0200

    add: exercises 5.10 Blog List Frontend, step10

--- part5/v4/bloglist-frontend/src/components/BlogList.jsx
+++ part5/v4/bloglist-frontend/src/components/BlogList.jsx
@@ -1,18 +1,26 @@
 import Blog from './Blog'
 
 const BlogList = ({ blogs, handleLike }) => {
+  // console.log('Initial', blogs)
+
+  // const sortedBlogs = blogs.sort((a,b) => a.likes - b.likes)
+
+  // console.log('Sorted', sortedBlogs)
+
   return (
     <>
       <h2>Blogs</h2>
       <ul>
         {
-          blogs.map(blog => (
-            <Blog
-              key={blog.id}
-              blog={blog}
-              handleLike={handleLike} 
-            />
-          ))
+          blogs
+            .sort((a,b) => b.likes - a.likes)
+            .map(blog => (
+              <Blog
+                key={blog.id}
+                blog={blog}
+                handleLike={handleLike} 
+              />
+            ))
         }
       </ul>
     </>
