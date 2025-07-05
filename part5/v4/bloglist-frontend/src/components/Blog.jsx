d09a01ab55e061ec0ffcfe3cfb8e310f4d503f15
Byron J Calvete <calvete94@gmail.com>
2025-07-05 13:37:43 +0200

    add: exercise 5.7 Blog List Frontend, step7

--- part5/v4/bloglist-frontend/src/components/Blog.jsx
+++ part5/v4/bloglist-frontend/src/components/Blog.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react'
+
 const Blog = ({ blog }) => {
-  const { title, author } = blog
+  const { title, author, url, likes } = blog
+
+  const [ showDetails, setShowDetails ] = useState(false)
+
+  const buttonLabel = showDetails ? 'hide' : 'view'
+
+  const hideWhenVisible = { display: showDetails ? 'none' : '' }
+  const showWhenVisible = { display: showDetails ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setShowDetails(!showDetails)
+  }
 
   return (
-    <li>{title} - {author}</li>
+    <li className='blog'>
+      <div style={hideWhenVisible}>
+        <p>{title} <button onClick={toggleVisibility}>{buttonLabel}</button></p>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{title} <button onClick={toggleVisibility}>{buttonLabel}</button></p>
+        <p>{url}</p>
+        <p>likes {likes}</p>
+        <p>{author}</p>
+      </div>
+    </li>
   )
 }
 
