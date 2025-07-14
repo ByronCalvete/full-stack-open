949fd8d0a2dc663ac202db8500201b5fa7585240
Byron J Calvete <calvete94@gmail.com>
2025-07-14 20:14:18 +0200

    add: exercise 6.2 Unicafe Revisited, step2

--- part6/v4/unicafe-redux/src/App.jsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div>
-      Hi, my friends!
-    </div>
-  )
-}
-
-export default App
--- part6/v4/unicafe-redux/src/main.jsx
+++ part6/v4/unicafe-redux/src/main.jsx
@@ -1,4 +1,31 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import { createStore } from 'redux'
+import counterReducer from "./reducers/counterReducer"
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button>
+      <button onClick={() => store.dispatch({ type: 'OK' })}>ok</button>
+      <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
+      <button onClick={() => store.dispatch({ type: 'ZERO' })}>reset stats</button>
+      <div>
+        <p>good {store.getState().good}</p>
+        <p>ok {store.getState().ok}</p>
+        <p>bad {store.getState().bad}</p>
+      </div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
