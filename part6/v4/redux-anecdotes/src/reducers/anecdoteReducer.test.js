85a93a3eea8ed405ffd155e9dc4d572d13a0d33d
Byron J Calvete <calvete94@gmail.com>
2025-07-17 17:34:23 +0200

    add: exercise 6.3 Anecdotes, step1

---
+++ part6/v4/redux-anecdotes/src/reducers/anecdoteReducer.test.js
@@ -0,0 +1,35 @@
+import { test, expect, describe } from 'vitest'
+import anecdoteReducer from './anecdoteReducer'
+import deepFreeze from 'deep-freeze'
+
+describe('anecdotes reducer', () => {
+  test('return new state with action NEW_VOTE', () => {
+    const state = [
+      {
+        content: 'The first anecdote for test',
+        id: 1,
+        votes: 3
+      },
+      {
+        content: 'The second anecdote for test',
+        id: 2,
+        votes: 1
+      }
+    ]
+    const action = {
+      type: 'NEW_VOTE',
+      payload: { id: 2 }
+    }
+
+    deepFreeze(state)
+    const newState = anecdoteReducer(state, action)
+
+    expect(newState).toHaveLength(2)
+    expect(newState).toContainEqual(state[0])
+    expect(newState).toContainEqual({
+      content: 'The second anecdote for test',
+      id: 2,
+      votes: 2
+    })
+  })
+})
