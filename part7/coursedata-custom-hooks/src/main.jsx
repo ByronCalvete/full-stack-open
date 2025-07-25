c10f0cecedb4d2644f05e5d3eb040bde0eab4694
Byron J Calvete <calvete94@gmail.com>
2025-07-26 00:59:12 +0200

    add: Custom Hooks, part1

---
+++ part7/coursedata-custom-hooks/src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+
+import AppOne from './AppOne'
+import AppTwo from './AppTwo'
+import AppThree from './AppThree'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <AppOne />
+  // <AppTwo />
+  <AppThree />
+)
