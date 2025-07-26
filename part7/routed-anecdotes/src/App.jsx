5ac096d2f07a3d711dc688d390910428f0b0bf43
Byron J Calvete <calvete94@gmail.com>
2025-07-26 16:26:53 +0200

    add: exercise 7.6 Anecdotes and Hooks, step3

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -66,9 +66,9 @@ const Notification = ({ message }) => {
 }
 
 const CreateNew = ({ addNew, setNotification }) => {
-  const content = useField('content')
-  const author = useField('author')
-  const info = useField('info')
+  const { reset: resetContent, ...content } = useField('content')
+  const { reset: resetAuthor, ...author } = useField('author')
+  const { reset: resetInfo, ...info} = useField('info')
 
   const navigate = useNavigate()
 
@@ -88,9 +88,9 @@ const CreateNew = ({ addNew, setNotification }) => {
   }
 
   const handleReset = () => {
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
