8feacb2c5e89e175917889d016706ab69744bce3
Byron J Calvete <calvete94@gmail.com>
2025-07-30 17:49:28 +0200

    fix: exercise 7.10 React Query and Context, step1D

---
+++ part7/bloglist-backend-react-query-context/controllers/user.js
@@ -0,0 +1,40 @@
+const usersRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response, next) => {
+  try {
+    const users = await User
+      .find({})
+      .populate('blogs', { url: 1, title: 1, author: 1, likes: 1 })
+    response.json(users)
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+usersRouter.post('/', async (request, response, next) => {
+  const { username, name, password } = request. body
+
+  try {
+    if (password.length < 3) {
+      return response.status(400).json({ error: 'password too short. Should have at least 3 characters' })
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+      username,
+      name,
+      passwordHash
+    })
+
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+module.exports = usersRouter
